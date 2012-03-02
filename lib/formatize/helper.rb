c42edd955a687b8fe580271de0145f710979e8e8
David Trasbo <me@dtrasbo.com>
2012-03-02 16:10:05 +0100

    Switch to YARD syntax

--- lib/formatize/helper.rb
+++ lib/formatize/helper.rb
@@ -1,6 +1,6 @@
 module Formatize
   module Helper
-    # Accepts a string of {Textile}(http://redcloth.org/textile) and one or
+    # Accepts a string of [Textile](http://redcloth.org/textile) and one or
     # more flags. Often, the default behavior will be suitable and you will not
     # have to pass it any flags:
     #
@@ -12,15 +12,15 @@ module Formatize
     #   textilize("(snip)", :flag1, :flag2, :flag3)
     #   # => "(snip)"
     #
-    # {The +RedCloth+ documentation}(http://redcloth.rubyforge.org/classes/RedCloth/TextileDoc.html)
+    # [The +RedCloth+ documentation](http://redcloth.rubyforge.org/classes/RedCloth/TextileDoc.html)
     # lists the available flags.
     #
     # It sanitizes the input using Rails' +sanitize+ prior to parsing.
     # There are two ways to bypass pre-parsing sanitization: 
     #
     # 1. Pass a string that has been marked HTML safe. (Preferred).
-    # 2. Use the special `:safe` flag, which is not passed on to the parser.
-    #    _(Deprecated in 1.1, removed in 2.0)._
+    # 2. Use the special <tt>:safe</tt> flag, which is not passed on to the
+    #    parser. _(Deprecated in 1.1, removed in 2.0)._
     #
     def textilize(text, *flags)
       require 'RedCloth'
@@ -48,7 +48,7 @@ module Formatize
       return textiled
     end
 
-    # Accepts a string of {Markdown}(http://daringfireball.net/projects/markdown/)
+    # Accepts a string of [Markdown](http://daringfireball.net/projects/markdown/)
     # and one or more flags. Often, the default behavior will be suitable and
     # you will not have to pass it any flags:
     #
@@ -60,15 +60,15 @@ module Formatize
     #   markdown("(snip)", :flag1, :flag2, :flag3)
     #   # => "(snip)"
     #
-    # {The +bluecloth+ documentation}(http://rubydoc.info/gems/bluecloth/BlueCloth)
+    # [The +bluecloth+ documentation](http://rubydoc.info/gems/bluecloth/BlueCloth)
     # lists the available flags.
     #
     # It sanitizes the input using Rails' +sanitize+ prior to parsing.
     # There are two ways to bypass pre-parsing sanitization: 
     #
     # 1. Pass a string that has been marked HTML safe. (Preferred).
-    # 2. Use the special `:safe` flag, which is not passed on to the parser.
-    #    _(Deprecated in 1.1, removed in 2.0)._
+    # 2. Use the special <tt>:safe</tt> flag, which is not passed on to the
+    #    parser. _(Deprecated in 1.1, removed in 2.0)._
     #
     def markdown(text, *flags)
       require 'bluecloth'
